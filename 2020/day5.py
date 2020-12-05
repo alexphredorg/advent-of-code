39e6bf23ab6ac6aa22178ce9c98da65f7be0d290
Alex Wetmore <alex@phred.org>
2020-12-05 06:39:53 -0800

    day5

---
+++ 2020/day5.py
@@ -0,0 +1,18 @@
+import string
+
+def readinput(filename):
+    binary_trans = str.maketrans("fbrlFBRL", "01100110")
+    with open(filename, 'r') as f:
+        seat_labels = f.readlines()
+    seat_ids = list(map(lambda x: 
+        int(x[0:7].translate(binary_trans), 2) * 8 + int(x[7:10].translate(binary_trans), 2), seat_labels))
+    return seat_ids
+
+def find_missing_seat(seat_set, max_value):
+    seats = [x for x in range(0, max_value) if x not in seat_set and x+1 in seat_set and x-1 in seat_set]
+    return seats[0]
+
+seat_ids = readinput("day5.txt")
+max_seat_id = max(seat_ids)
+print("Max seat ID is %d " % max_seat_id)
+print("Your seat is %d" % find_missing_seat(set(seat_ids), max_seat_id))
